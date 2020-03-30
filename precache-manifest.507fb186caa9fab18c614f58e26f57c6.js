b4456de6249765c1c034c221761e913b939c8c9b
arian <ariannargesi@gmail.com>
2020-03-31 01:34:18 +0430

    Updates

---
+++ precache-manifest.507fb186caa9fab18c614f58e26f57c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4353f3586d8275446fd7d036ab65681f",
+    "url": "/mash-rejab-cannabis-shop/index.html"
+  },
+  {
+    "revision": "6e87d6af41b201d83e19",
+    "url": "/mash-rejab-cannabis-shop/static/css/main.60ec9315.chunk.css"
+  },
+  {
+    "revision": "a3212b5b10d7b9b9f70c",
+    "url": "/mash-rejab-cannabis-shop/static/js/2.8198f056.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mash-rejab-cannabis-shop/static/js/2.8198f056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e87d6af41b201d83e19",
+    "url": "/mash-rejab-cannabis-shop/static/js/main.9416320b.chunk.js"
+  },
+  {
+    "revision": "7284ab0279c2f35ce863",
+    "url": "/mash-rejab-cannabis-shop/static/js/runtime-main.ff54ec9c.js"
+  }
+]);
